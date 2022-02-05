794ba05cbb9fc1eabc5365d304f2fe48dc86c89a
Alexandr Atamanov <pustato.aas@gmail.com>
2022-02-05 13:51:24 +0300

    HW05 Done

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -66,5 +67,76 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+}
+
+func TestWithoutTimout(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	t.Run("m=0 means ignore errors", func(t *testing.T) {
+		n, m, tasksCount := 11, 0, 103
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		waitCh := make(chan struct{})
+		var err error
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				<-waitCh
+
+				return errors.New("some error")
+			})
+		}
+
+		lock := make(chan struct{})
+		go (func() {
+			defer close(lock)
+
+			err = Run(tasks, n, m)
+		})()
+
+		require.Eventuallyf(t, func() bool {
+			return int(atomic.LoadInt32(&runTasksCount)) == n
+		}, time.Second, time.Microsecond, "workers is not parallel")
+
+		close(waitCh)
+
+		<-lock
+		require.NoError(t, err, "errors were not ignored")
+		require.Equal(t, int(runTasksCount), tasksCount, "not all tasks were completed")
+	})
+
+	t.Run("edge case with single long error task", func(t *testing.T) {
+		n, m, tasksCount := 7, 1, 61
+		tasks := make([]Task, 0, tasksCount)
+
+		var successTasksCount, errorTasksCount int32
+		var successTasksWg sync.WaitGroup
+
+		tasks = append(tasks, func() error {
+			atomic.AddInt32(&errorTasksCount, 1)
+			successTasksWg.Wait()
+
+			return errors.New("some error")
+		})
+
+		for i := 1; i < tasksCount; i++ {
+			successTasksWg.Add(1)
+			tasks = append(tasks, func() error {
+				defer successTasksWg.Done()
+
+				atomic.AddInt32(&successTasksCount, 1)
+
+				return nil
+			})
+		}
+
+		err := Run(tasks, n, m)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.Equal(t, int32(tasksCount-1), successTasksCount, "actual success tasks: %d", successTasksCount)
+		require.Equal(t, int32(1), errorTasksCount, "actual error tasks: %d", errorTasksCount)
 	})
 }
